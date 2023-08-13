fdac7acd635724a54d146c24bc8104fbe4880fb1
Julia <malukjulia2014@gmail.com>
2023-08-13 07:51:31 +0000

    Deployment

--- blog/admin.py
+++ blog/admin.py
@@ -4,13 +4,15 @@ from django.contrib.auth.models import User
 from .models import Post, Comment, CallBooking
 from django_summernote.admin import SummernoteModelAdmin
 
+# Customizing the UserAdmin to display the date joined
 class CustomUserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('date_joined',)
 
+# Unregister the default User admin and register the custom one
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
-
+# Customizing the admin interface for the Post model
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
@@ -19,6 +21,7 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+# Customizing the admin interface for the Comment model
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
@@ -29,6 +32,7 @@ class CommentAdmin(admin.ModelAdmin):
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
 
+# Customizing the admin interface for the CallBooking model
 @admin.register(CallBooking)
 class CallBookingAdmin(admin.ModelAdmin):
     list_display = ('id', 'client_name', 'client_email', 'phone_number', 'details', 'call_data', 'user', 'created_on')  
